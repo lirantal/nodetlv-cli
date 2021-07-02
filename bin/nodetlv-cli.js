8bf8d215f9112a9d931463a9a1e2565a8d615246
Liran Tal <liran.tal@gmail.com>
2021-07-02 12:17:13 +0300

    feat: progress bar and intro music playing

--- bin/nodetlv-cli.js
+++ bin/nodetlv-cli.js
@@ -1,10 +1,68 @@
 #!/usr/bin/env node
 
 const path = require('path')
+const ProgressBar = require('cli-progress')
+const colors = require('colors')
 const soundPlayer = require('play-sound')()
 
-const INTRO_FILE = path.join(__dirname, '../', 'assets', 'bensound-dubstep.mp3')
+const INTRO_MUSIC_FILE = path.join(__dirname, '../', 'assets', 'bensound-dubstep.mp3')
+const TIMER_SKIPS_MUSIC_LOADER = 18
+const TIMER_SKIPS_VIBES_LOADER = 14
 
-const soundHandler = soundPlayer.play(INTRO_FILE)
+function musicLoader() {
+  return new Promise((resolve, reject) => {
+    const progressBar = new ProgressBar.Bar({
+      format: `Loading music      | ${colors.cyan('{bar}')} | {percentage}%`,
+      barCompleteChar: '\u2588',
+      barIncompleteChar: '\u2591',
+      hideCursor: false
+    })
 
-console.log('Nothing yet to see here...')
+    let value = 2
+    progressBar.start(200, 0)
+    const progressBarTimer = setInterval(() => {
+      value++
+      progressBar.update(value)
+      if (value >= progressBar.getTotal()) {
+        clearInterval(progressBarTimer)
+        progressBar.stop()
+        return resolve()
+      }
+    }, TIMER_SKIPS_MUSIC_LOADER)
+  })
+}
+
+function goodVibesLoader() {
+  return new Promise((resolve, reject) => {
+    const progressBar = new ProgressBar.Bar({
+      format: `Loading good vibes | ${colors.magenta('{bar}')} | {percentage}%`,
+      barCompleteChar: '\u2588',
+      barIncompleteChar: '\u2591',
+      hideCursor: false
+    })
+
+    let value = 2
+    progressBar.start(200, 0)
+    const progressBarTimer = setInterval(() => {
+      value++
+      progressBar.update(value)
+      if (value >= progressBar.getTotal()) {
+        clearInterval(progressBarTimer)
+        progressBar.stop()
+        return resolve()
+      }
+    }, TIMER_SKIPS_VIBES_LOADER)
+  })
+}
+
+async function init() {
+  await musicLoader()
+  const soundHandler = soundPlayer.play(INTRO_MUSIC_FILE)
+  await goodVibesLoader()
+
+  process.on('exit', () => {
+    soundHandler.kill()
+  })
+}
+
+init()
